c2c6532789b330b8bd6ac8d03e2203b8788d0405
Rafał Łasocha <me+dev@swistak35.com>
2015-05-26 13:56:05 +0200

    up

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -3,18 +3,18 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (concat (recursive-reverse (rest coll)) (list (first coll))))
 
 (defn factorial [n]
   __)
